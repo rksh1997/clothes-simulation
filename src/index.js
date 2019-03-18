7c406c149063f352a06070dd5674e5a5ab5b7151
Rashad Kokash <richardeo112@gmail.com>
2019-03-18 05:51:55 +0200

    test three js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-import greet from './test'
+import {
+	Scene,
+	PerspectiveCamera,
+	WebGLRenderer,
+    BoxGeometry,
+    MeshBasicMaterial,
+    Mesh
+} from 'three';
 
-console.log(greet('Rashad'))
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
+
+const scene = new Scene()
+const camera = new PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 100);
+const renderer = new WebGLRenderer()
+
+renderer.setSize(WIDTH, HEIGHT);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new BoxGeometry( 1, 1, 1 );
+const material = new MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new Mesh( geometry, material );
+
+scene.add( cube );
+camera.position.z = 5;
+
+function animate() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+}
+animate();
